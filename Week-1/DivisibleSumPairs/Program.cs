3ae3cae36d02d06d1d2d88b7554b2278c013b7cf
Сігута Діана <diana.sihuta@nure.ua>
2023-03-06 11:59:20 +0200

    Configure new project to solve the 'Divisible Sum Pairs' task

---
+++ Week-1/DivisibleSumPairs/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DivisibleSumPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var inputArr = input?.Split(' ');
+
+            int n = Convert.ToInt32(inputArr?[0]);
+            int k = Convert.ToInt32(inputArr?[1]);
+
+            input = Console.ReadLine();
+            var intList = input?
+                .Split(' ')
+                .Select(i => Convert.ToInt32(i))
+                .ToList();
+
+            int res = Result.divisibleSumPairs(n, k, intList!);
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ Week-1/DivisibleSumPairs/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace DivisibleSumPairs
+{
+    class Result
+    {
+        /*
+         * Complete the 'divisibleSumPairs' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER k
+         *  3. INTEGER_ARRAY arr
+         */
+
+        public static int divisibleSumPairs(int n, int k, List<int> arr)
+        {
+            return 0;
+        }
+
+    }
+}
