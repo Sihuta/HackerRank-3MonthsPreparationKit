e608bf60886f8abbe1c5059b68b3de702eac25c3
Сігута Діана <diana.sihuta@nure.ua>
2023-03-07 11:26:26 +0200

    Solve the 'Sparse Arrays' problem

---
+++ Week-1/SparseArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SparseArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n = Convert.ToInt32(Console.ReadLine());
+            var strings = new List<string>(n);
+
+            for (int i = 0; i < n; ++i)
+            {
+                strings.Add(Console.ReadLine()!);
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            var queries = new List<string>(n);
+
+            for (int i = 0; i < n; ++i)
+            {
+                queries.Add(Console.ReadLine()!);
+            }
+
+            var res = Result.matchingStrings(strings, queries);
+            Console.WriteLine(string.Join("\n", res));
+        }
+    }
+}
---
+++ Week-1/SparseArrays/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace SparseArrays
+{
+    class Result
+    {
+        /*
+         * Complete the 'matchingStrings' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY strings
+         *  2. STRING_ARRAY queries
+         */
+
+        public static List<int> matchingStrings(List<string> strings, List<string> queries)
+            => queries.Select(q => strings.Count(s => s.Equals(q))).ToList();
+
+    }
+}
