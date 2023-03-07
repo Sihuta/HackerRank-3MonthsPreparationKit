d8baee40bfeb714da0fe2345c4cd3dc5bb118d31
Сігута Діана <diana.sihuta@nure.ua>
2023-03-07 11:48:19 +0200

    Solve the 'Lonely Integer' problem

---
+++ Week-2/LonelyInteger/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace LonelyInteger
+{
+    class Result
+    {
+        /*
+         * Complete the 'lonelyinteger' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+
+        public static int lonelyinteger(List<int> a)
+            => a.Find(x => a.Where(y => y == x).Count() == 1);
+    }
+}
