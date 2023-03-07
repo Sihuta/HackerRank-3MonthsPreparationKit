ba50bb71e2a4df631c6aa1f42bf8271d4e6c8d96
Сігута Діана <diana.sihuta@nure.ua>
2023-03-07 12:03:09 +0200

    Solve the 'Grading Students' problem

---
+++ Week-2/GradingStudents/Result.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata;
+
+namespace GradingStudents
+{
+    class Result
+    {
+        /*
+         * Complete the 'gradingStudents' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY grades as parameter.
+         */
+
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            const int limit = 3;
+            const int falling = 39;
+
+            return grades.Select(g =>
+            {
+                int rounded = getRounded(g);
+                if (rounded > falling &&
+                    rounded - g < limit)
+                {
+                    return rounded;
+                }
+                return g;
+            }).ToList();
+        }
+
+        private static int getRounded(int grade)
+            => (grade / 5 + 1) * 5;
+    }
+}
