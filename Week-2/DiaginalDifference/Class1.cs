c02e23117bb6c28ef8cfe2ca0358474067dd174f
Сігута Діана <diana.sihuta@nure.ua>
2023-03-13 14:28:04 +0200

    Solve the 'Diagonal Difference' problem

---
+++ Week-2/DiaginalDifference/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace DiaginalDifference
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int principal = 0;
+            int secondary = 0;
+
+            for (int i = 0; i < arr.Count; ++i)
+            {
+                principal += arr[i][i];
+                secondary += arr[i][arr.Count - i - 1];
+            }
+
+            return Math.Abs(principal - secondary);
+        }
+
+    }
+}
