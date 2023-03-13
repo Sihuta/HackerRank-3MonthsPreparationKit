aea6e0ad4b2a7e4b62d017d1029c3d9fc0ab677d
Сігута Діана <diana.sihuta@nure.ua>
2023-03-13 14:58:27 +0200

    Solve the 'Counting Sort' problem

---
+++ Week-2/CountingSort-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CountingSort_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var input = Console.ReadLine()?
+                .Split(' ')
+                .Select(x => Convert.ToInt32(x))
+                .ToList();
+
+            var output = Result.countingSort(input!);
+            Console.WriteLine(string.Join(' ', output));
+        }
+    }
+}
---
+++ Week-2/CountingSort-1/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace CountingSort_1
+{
+    class Result
+    {
+        /*
+         * Complete the 'countingSort' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static List<int> countingSort(List<int> arr)
+        {
+            const int n = 100;
+            int[] res = new int[n];
+
+            for (int i = 0; i < arr.Count; ++i)
+            {
+                int ind = arr[i];
+                res[ind] += 1;
+            }
+
+            return res.ToList();
+        }
+
+    }
+}
