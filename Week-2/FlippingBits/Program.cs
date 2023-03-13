2d1b8e65a531594343ab639bf813dae0c17bd5df
Сігута Діана <diana.sihuta@nure.ua>
2023-03-13 14:06:26 +0200

    Solve the 'Flipping Bits' problem

---
+++ Week-2/FlippingBits/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FlippingBits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; ++i)
+            {
+                long input = Convert.ToInt64(Console.ReadLine());
+                long output = Result.flippingBits(input);
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ Week-2/FlippingBits/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace FlippingBits
+{
+    class Result
+    {
+        /*
+         * Complete the 'flippingBits' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER n as parameter.
+         */
+
+        public static long flippingBits(long n)
+        {
+            string bits = ToBinStr(n);
+            var inverted = string.Join("", bits
+                .Select(b => b == '0' ? '1' : '0')
+                .ToArray());
+
+            return ToLong(new string(inverted));
+        }
+
+        private static string ToBinStr(long n)
+            => Convert.ToString(n, 2).PadLeft(32, '0');
+
+        private static long ToLong(string? bits)
+            => Convert.ToInt64(bits, 2);
+    }
+}
