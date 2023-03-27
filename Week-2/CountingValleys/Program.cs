b1c04d355dec66088929085ac9762e480472be5b
Сігута Діана <diana.sihuta@nure.ua>
2023-03-27 18:27:25 +0300

    Solve the 'Counting Valleys' problem

---
+++ Week-2/CountingValleys/Program.cs
@@ -0,0 +1,4 @@
+﻿using CountingValleys;
+
+int res = Result.countingValleys(8, "UDDDUDUU");
+Console.WriteLine(res);
---
+++ Week-2/CountingValleys/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace CountingValleys
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'countingValleys' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER steps
+         *  2. STRING path
+         */
+
+        public static int countingValleys(int steps, string path)
+        {
+            int valleysCount = 0;
+            int level = 0;
+
+            foreach (var step in path)
+            {
+                bool stepUp = step == 'U';
+                level = stepUp ? level + 1 : level - 1;
+
+                if (stepUp && level == 0)
+                {
+                    ++valleysCount;
+                }
+            }
+
+            return valleysCount;
+        }
+    }
+}
