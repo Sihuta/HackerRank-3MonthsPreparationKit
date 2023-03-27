f25bb7d36edc91e2180851d0c14eb3fd5f15e139
Сігута Діана <diana.sihuta@nure.ua>
2023-03-27 18:39:49 +0300

    Solve the 'Pangrams' problem

---
+++ Week-2/Pangrams/Program.cs
@@ -0,0 +1,4 @@
+﻿using Pangrams;
+
+var res = Result.pangrams("We promptly judged antique ivory buckles for the next prize");
+Console.WriteLine(res);
---
+++ Week-2/Pangrams/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace Pangrams
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'pangrams' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string pangrams(string s)
+        {
+            const string enAlphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+            foreach (var letter in enAlphabet.Split(' '))
+            {
+                if (!s.Contains(letter) && !s.Contains(letter.ToUpper()))
+                {
+                    return "not pangram";
+                }
+            }
+
+            return "pangram";
+        }
+    }
+}
