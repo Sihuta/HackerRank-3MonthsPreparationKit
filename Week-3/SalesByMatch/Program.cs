8e4b5c51977e1af6d35449f1c9654a8dfbeb846f
Сігута Діана <diana.sihuta@nure.ua>
2023-03-28 15:57:48 +0300

    Solve the 'Sales by Match' problem

---
+++ Week-3/SalesByMatch/Program.cs
@@ -0,0 +1,4 @@
+﻿using SalesByMatch;
+
+var res = Result.sockMerchant(9, new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+Console.WriteLine(res);
---
+++ Week-3/SalesByMatch/Result.cs
@@ -0,0 +1,31 @@
+﻿namespace SalesByMatch
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'sockMerchant' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER_ARRAY ar
+         */
+
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int pairsCount = 0;
+            for (int i = 0; i < ar.Count; ++i)
+            {
+                int lastInd = ar.LastIndexOf(ar[i]);
+                if (lastInd != i)
+                {
+                    ++pairsCount;
+                    ar.RemoveAt(i--);
+                    ar.RemoveAt(lastInd - 1);
+                }
+            }
+
+            return pairsCount;
+        }
+    }
+}
