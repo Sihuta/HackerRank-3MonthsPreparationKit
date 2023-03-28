654219116997ea23ed7de3fd931a537fb65eb1d7
Сігута Діана <diana.sihuta@nure.ua>
2023-03-28 15:11:43 +0300

    Solve the 'Flipping Matrix' problem assigned for the Mock Test

---
+++ Week-2/MockTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using MockTest;
+
+int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+for (int qItr = 0; qItr < q; qItr++)
+{
+    int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+    List<List<int>> matrix = new List<List<int>>();
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        matrix.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+    }
+
+    int result = Result.flippingMatrix(matrix);
+    Console.WriteLine(result);
+}
---
+++ Week-2/MockTest/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace MockTest
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'flippingMatrix' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+         */
+
+        public static int flippingMatrix(List<List<int>> matrix)
+        {
+            int n = matrix.Count;
+            int maxSum = 0;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    int upperLeft = matrix[i][j];
+                    int upperRight = matrix[i][n - j - 1];
+                    int lowerLeft = matrix[n - i - 1][j];
+                    int lowerRight = matrix[n - i - 1][n - j - 1];
+
+                    maxSum += Math.Max(Math.Max(upperLeft, upperRight), Math.Max(lowerLeft, lowerRight));
+
+                    matrix[i][j] = lowerRight;
+                    matrix[i][n - j - 1] = lowerLeft;
+                    matrix[n - i - 1][j] = upperRight;
+                    matrix[n - i - 1][n - j - 1] = upperLeft;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
