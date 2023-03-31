a49af4cce35344d68b8506d4ad9457fad6329865
Сігута Діана <diana.sihuta@nure.ua>
2023-03-31 15:45:54 +0300

    Solve the 'Migratory Birds' problem

---
+++ Week-3/MigratoryBirds/Program.cs
@@ -0,0 +1,4 @@
+﻿using MigratoryBirds;
+
+var res = Result.migratoryBirds(new List<int> { 1, 1, 2, 2, 3 });
+Console.WriteLine(res);
---
+++ Week-3/MigratoryBirds/Result.cs
@@ -0,0 +1,35 @@
+﻿namespace MigratoryBirds
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'migratoryBirds' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int migratoryBirds(List<int> arr)
+        {
+            var frequencyArr = new int[arr.Count];
+            int maxFrequency = 0;
+            int mostFrequent = arr[0];
+
+            foreach (int num in arr)
+            {
+                ++frequencyArr[num];
+                if (frequencyArr[num] > maxFrequency)
+                {
+                    maxFrequency = frequencyArr[num];
+                    mostFrequent = num;
+                }
+                else if (frequencyArr[num] == maxFrequency && num < mostFrequent)
+                {
+                    mostFrequent = num;
+                }
+            }
+
+            return mostFrequent;
+        }
+    }
+}
