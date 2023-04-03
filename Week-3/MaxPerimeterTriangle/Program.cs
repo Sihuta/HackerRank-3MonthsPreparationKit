1e93b73950a2e50674f90e6d6c70ef8a0e9b4b63
Сігута Діана <diana.sihuta@nure.ua>
2023-04-03 08:28:56 +0300

    Solve the 'Max Perimeter Triangle' problem

---
+++ Week-3/MaxPerimeterTriangle/Program.cs
@@ -0,0 +1,7 @@
+﻿using MaxPerimeterTriangle;
+
+var res = Result.maximumPerimeterTriangle(new List<int> { 1, 2, 3, 4, 5, 10 });
+foreach (var n in res)
+{
+    Console.Write(n + " ");
+}
---
+++ Week-3/MaxPerimeterTriangle/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxPerimeterTriangle
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'maximumPerimeterTriangle' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY sticks as parameter.
+         */
+
+        public static List<int> maximumPerimeterTriangle(List<int> sticks)
+        {
+            var orderedSticks = sticks.OrderBy(x => x).ToList();
+            for (int i = orderedSticks.Count - 1; i >= 2; --i)
+            {
+                int a = orderedSticks[i];
+                int b = orderedSticks[i - 1];
+                int c = orderedSticks[i - 2];
+
+                if (a < b + c)
+                {
+                    return new List<int> { c, b, a };
+                }
+            }
+
+            return new List<int> { -1 };
+        }
+    }
+}
