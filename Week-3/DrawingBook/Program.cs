16736a2ceecc18e154ade7599d24014c0e7f2c10
Сігута Діана <diana.sihuta@nure.ua>
2023-04-03 08:47:54 +0300

    Solve the 'Drawing Book' problem

---
+++ Week-3/DrawingBook/Program.cs
@@ -0,0 +1,4 @@
+﻿using DrawingBook;
+
+var res = Result.pageCount(5, 3);
+Console.WriteLine(res);
---
+++ Week-3/DrawingBook/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace DrawingBook
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'pageCount' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER p
+         */
+
+        public static int pageCount(int n, int p)
+        {
+            int fromStart = p - 1;
+            int fromEnd = n - p;
+
+            if (fromStart < fromEnd)
+            {
+                return (fromStart + 1) / 2;
+            }
+
+            return fromEnd / 2;
+        }
+    }
+}
