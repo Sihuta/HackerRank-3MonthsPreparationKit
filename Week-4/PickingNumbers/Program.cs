bf789092bcf17f5cbafb9296fbb6fe21aac7913a
Сігута Діана <diana.sihuta@nure.ua>
2023-04-04 14:57:54 +0300

    Solve the 'Picking Numbers' problem

---
+++ Week-4/PickingNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿using PickingNumbers;
+
+var n = Console.ReadLine();
+var stringArr = Console.ReadLine()?.Split(' ');
+if (stringArr != null)
+{
+    var list = Array.ConvertAll(stringArr, int.Parse).ToList();
+    var res = Result.pickingNumbers(list);
+    Console.WriteLine(res);
+}
---
+++ Week-4/PickingNumbers/Result.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PickingNumbers
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'pickingNumbers' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+
+        public static int pickingNumbers(List<int> list)
+        {
+            int len = 1;
+            int maxLen = len;
+            int subsetStartInd = 0;
+
+            var orderedList = list.OrderBy(x => x).ToList();
+            for (int i = 1; i < orderedList.Count; ++i)
+            {
+                if (Math.Abs(orderedList[i] - orderedList[i - 1]) <= 1 &&
+                    Math.Abs(orderedList[i] - orderedList[subsetStartInd]) <= 1)
+                {
+                    ++len;
+                }
+                else
+                {
+                    subsetStartInd = i;
+                    maxLen = Math.Max(maxLen, len);
+                    len = 1;
+                }
+            }
+
+            return Math.Max(maxLen, len);
+        }
+    }
+}
